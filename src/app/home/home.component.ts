d7d63eb2c5e17e85b438d0c67c66356650a7eadf
Cuitlahuac A <probandomichat@gmail.com>
2018-12-18 11:10:02 -0600

    Obtención de valores dinámico utilizando los date picker

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import { CurrencyService } from '../services/currency.service';
+import * as moment from 'moment'; 
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,46 @@ import { CurrencyService } from '../services/currency.service';
 export class HomeComponent implements OnInit {
 
   public chart: any;
+  public start_date: any;
+  public end_date: any;
+  public values: any[] = [];
+
   constructor(private _currencyService: CurrencyService) { }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData(start_date = undefined, end_date = undefined) {
+    this.values = [];
+    this._currencyService.getDateRange(start_date, end_date).subscribe(data => {
+      console.log(data.results.USD_MXN.val);
+      let dd = data.results.USD_MXN.val;
+      for(let date in dd) {
+	console.log('Fecha ' + date +': ', dd[date]);
+	this.values.push(dd[date]);
+      }
+      console.log('Valores: ', this.values);
+      this.drawChart();
+    });
+  }
+
+  setDates(){
+    let start_date = moment(this.start_date).format("YYYY-MM-DD");
+    let end_date = moment(this.end_date).format("YYYY-MM-DD");
+
+    if(moment(this.end_date).subtract(7,'days') > this.start_date){
+      alert('Solo es posible consultar un rango de 8 días');
+      return;
+    }
+
+    this.getData(start_date, end_date);
+  }
+
+  drawChart(){
     this.chart = new Chart({
       chart: {
-        type: 'line'
+        type: 'area'
       },
       title: {
         text: 'Linechart'
@@ -25,22 +60,10 @@ export class HomeComponent implements OnInit {
       },
       series: [
         {
-          name: 'Line 1',
-          data: [1, 2, 3]
+          name: 'MXN',
+          data: this.values
         }
       ]
     });
-    this.getData();
-  }
-
-  getData() {
-    this._currencyService.getDateRange().subscribe(data => {
-      console.log(data);
-    });
-  }
-
-  // add point to chart serie
-  add() {
-    this.chart.addPoint(Math.floor(Math.random() * 10));
   }
 }
