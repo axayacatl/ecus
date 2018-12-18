f9f28dfc6b5435d9a28adb4d7105b1ad857e94e6
Cuitlahuac A <probandomichat@gmail.com>
2018-12-18 00:30:36 -0600

    Se agregó datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartModule } from 'angular-highcharts';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DatepickerModule } from 'angular7-material-datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { CurrencyService } from './services/currency.service'
@@ -14,6 +16,8 @@ import { CurrencyService } from './services/currency.service'
     BrowserModule,
     ChartModule,
     HttpClientModule,
+    DatepickerModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [
